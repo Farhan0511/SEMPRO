7646b17c009636b6d726a7ba1e5678f5851ff962
Farhan0511 <programingsukses.com>
2024-11-30 09:53:57 +0700

    add student views

---
+++ src/views/Student.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <navbar/>
+  
+  <div>
+    <h2>This is Student views</h2>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components: { Navbar },
+    name: "Student"
+}
+</script>
+
+<style>
+
+</style>
